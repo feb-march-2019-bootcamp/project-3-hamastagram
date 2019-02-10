078c2e2780041aff5cf70418a5e9c4b8fb50ef18
hamastagram <kurdb14@gmail.com>
2019-02-10 03:26:08 +0300

    palindrome checker

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+var str = "aba";
 
+let strClean = str.replace(/[^a-zA-Z ]/g, "");
+
+strClean = strClean.replace(/\s/g, '')
+// console.log(strClean);
+
+
+if( strClean == strClean.split('').reverse().join('')){
+    console.log(strClean +" is palindrome");
+}
+else console.log(strClean + " not palindrome");
